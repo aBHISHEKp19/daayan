9c58a91fb7ff4aab4b6d4b80af9e514e8a219a49
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-01-19 13:42:19 +0530

    gy

--- commands/join.js
+++ commands/join.js
@@ -5,20 +5,26 @@ module.exports = {
     name: 'join',
     description: 'Joins ',
     async execute(message, args) {
- 
+
 
         if (!joinVoiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
         if (!args[0]) return message.channel.send("lol");
 
 
-
         const connection = joinVoiceChannel({
+
+            selfDeaf: message.guild.me.voice.setDeaf(false),
             channelId: message.member.voice.channel.id,
+
             guildId: message.guild.id,
             adapterCreator: message.guild.voiceAdapterCreator,
-        });
 
-       // const connection = await channel.join();
+        })
+
+        // connection.setDeaf(false);
+        // message.guild.me.voice.setDeaf(true, 
+
+        // const connection = await channel.join();
 
 
     }
