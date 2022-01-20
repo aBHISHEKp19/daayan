894f0ef0053bfb08461a09b3eb6a7c4bd3204e86
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-01-20 17:37:31 +0530

    sshvs

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ client.on('messageCreate', message => {
     client.on('messageCreate', message => {
 
         if (message.mentions.has(client.user.id)) {
-            message.channel.send("**DAAYAN HUN MAI**!");
+            message.channel.send("**DAAYAN HUN MAI**!!!");
         }
     })
 
