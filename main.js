7c1ff33288e147f0f9a1c88ba291780bd47cb0e6
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-01-22 17:33:56 +0530

    jcxnh

--- main.js
+++ main.js
@@ -36,6 +36,21 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', message => {
+    if (message.content === prefix + 'ping') {
+
+        // msg.delete()
+        const lil = new Discord.MessageEmbed()
+
+            .setDescription("**STATUS**")
+            .addField('Message Latency🎈', `${Date.now() - message.createdTimestamp}ms`)
+            .addField('API Latency🎈', `${client.ws.ping}ms`)
+            .setTimestamp()
+        message.channel.send({ embeds: [lil] });
+
+    }
+
+});
+client.on('messageCreate', message => {
 
 
     if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -64,6 +79,60 @@ client.on('messageCreate', message => {
         if (message.mentions.has(client.user.id)) {
             message.channel.send("**DAAYAN HUN MAI** *G maar dungi* !!!");
         }
+      
+     /*   if (message.content === 'Hi') {
+
+            message.channel.send("**TU CHUP SAALE <a:spookyghost:934414566845198356>**");
+        }*/
+
+        if (message.content === 'chup') {
+
+            message.channel.send("**TU CHUP SAALE <a:spookyghost:934414566845198356>**");
+        }
+
+
+        if (message.content === 'meri h') {
+
+            message.channel.send("**Mai sirf `APPY` Ki hun <a:ghostu:934414227026878496>**");
+        }
+
+        if (message.content === 'appy') {
+
+            message.channel.send("**I LOVE YOU appu <a:ghostu:934414227026878496>**");
+        }
+
+        if (message.content === 'kushal') {
+
+            message.channel.send("**Yes baby? <a:ghostu:934414227026878496>**");
+        } 
+
+        if (message.content === 'ily') {
+
+            message.channel.send("**I LOVE YOU TOO <a:ghostu:934414227026878496>**");
+        }
+        if (message.content === 'ansh') {
+
+            message.channel.send("**Vo chutiya hai <a:ghostu:934414227026878496>**");
+        }
+        if (message.content === 'nandini') {
+
+            message.channel.send("**NANDU SABKA BANDU <a:ghostu:934414227026878496>**");
+        }
+
+
+        if (message.content === 'kirti') {
+
+            message.channel.send("**CHAMELI HAI VO <a:ghostu:934414227026878496>**");
+        }
+
+       
+    /*    if (message.content === 'Rule 1') {
+
+           message.channel.send(`**▸Use channels for the thing they are made for,  this includes (but not limited to) media channel for sending media, chat Channel for chatting, Bot commands for using Commands 
+     Punishment: 3h Mute | 1d Hardmute | 7d kick | Ban**`);
+        }  */
+
+
     })
 
 /*
